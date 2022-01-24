e591cc3bc0362d7db39686412f6a6d9df5a99747
Vidhi Parekh <59387943+vidhi20@users.noreply.github.com>
2022-01-24 17:08:28 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+# ExpenseTracker
+An Expense Tracker Website made using Django
+
+An Expesne Tracker Website made using the Django Framework.
+
+It allows useres to input their Expenses and Income and helps them visualize it using Charts, 
+which leads to making better decisions about how much more to spend, invest and save. 
+
+And it also exports the records in PDF, Excel and CSV formats. 
+
+![ss2](https://github.com/priyaankparekh/ExpenseTracker/blob/main/screenshots/ss2.png)
+
+![ss3](https://github.com/priyaankparekh/ExpenseTracker/blob/main/screenshots/ss3.png)
+
+![ss4](https://github.com/priyaankparekh/ExpenseTracker/blob/main/screenshots/ss4.png)
---
+++ static/js/statsIncome.js
@@ -0,0 +1,56 @@
+const renderChart = (data, labels) => {
+  var ctx = document.getElementById("myChart").getContext("2d");
+  var myChart = new Chart(ctx, {
+    type: "pie",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: "Last 6 months Income",
+          data: data,
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)",
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      title: {
+        display: true,
+        text: "Income per category",
+      },
+    },
+  });
+};
+
+const getChartData = () => {
+  console.log("fetching");
+  fetch("/income_source_summary")
+    .then((res) => res.json())
+    .then((results) => {
+      console.log("results", results);
+      const source_data = results.income_source_data;
+      const [labels, data] = [
+        Object.keys(source_data),
+        Object.values(source_data),
+      ];
+
+      renderChart(data, labels);
+    });
+};
+
+document.onload = getChartData();
